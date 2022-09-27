9b83646022a02431dbd8d2dbe04da4e92503a90a
Eurêka <therealeureka@protonmail.com>
2022-09-27 11:36:00 +0200

    Route twig

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
@@ -11,12 +11,23 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 
-$twig = Twig::create('./view', ['cache' => false]);
-$app->add(TwigMiddleware::create($app, $twig));
+
+$twig = Twig::create('view/', ['cache' => false]);
 
 $app = AppFactory::create();
 
+$app->add(TwigMiddleware::create($app, $twig));
+
+
 error_reporting(E_ALL ^ E_DEPRECATED);
+
+$app->get('/twig/{id}',
+    function (Request $rq, Response $rs, array $args) : Response {
+       $view = Twig::fromRequest($rq);
+       return $view->render($rs, 'test.html', [
+            'id' => $args['id']
+        ]);
+    });
 
 $app->get('/',
     function (Request $rq, Response $rs): Response {
