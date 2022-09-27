8c3ce24af17ba7eb5182514c47381e8e03bbc199
Eurêka <therealeureka@protonmail.com>
2022-09-27 10:51:28 +0200

    Aled

--- public/index.php
+++ public/index.php
@@ -6,19 +6,10 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-/**
- * Instantiate App
- *
- * In order for the factory to work you need to ensure you have installed
- * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
- * ServerRequest creator (included with Slim PSR-7)
- */
 $app = AppFactory::create();
 
 error_reporting(E_ALL ^ E_DEPRECATED);
 
-
-//DEFAULT
 $app->get('/',
     function (Request $rq, Response $rs): Response {
         $rs->getBody()->write("Aled");
