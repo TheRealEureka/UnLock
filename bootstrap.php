621320333cbcb643a5fe7b072b042312a485e910
Eureka <therealeureka@protonmail.com>
2022-10-26 16:35:28 +0200

    Enable cache + clear logs

--- bootstrap.php
+++ bootstrap.php
@@ -53,8 +53,7 @@ $container->set(EntityManager::class, static function (Container $c): EntityMana
 $container->set('view', function () {
     return Twig::create(
         __DIR__ . '/public/view',
-        ['cache' => false]
-        //['cache' => __DIR__ . '/cache']
+        ['cache' => __DIR__ . '/cache']
     );
 });
 $container->set(CardService::class, static function (Container $c) {
