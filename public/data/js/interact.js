5e4aced136529cf2d584ff2184e9a93be7413533
Eurêka <therealeureka@protonmail.com>
2022-10-27 13:46:40 +0200

    🦝🦝 Calculator 🦝🦝

--- public/data/js/interact.js
+++ public/data/js/interact.js
@@ -16,3 +16,51 @@ document.getElementById("saveButton").addEventListener("click", function() {
     window.location = "/save";
 
 });
+
+
+//calculator
+let html = `
+<html lang="fr">
+<head>
+<link rel="stylesheet" href="../data/style/calculator.css">
+<title>Calculator</title>
+</head>
+<body>
+    <div class="calculator">
+      <input type="text" class="calculator-screen" value="" disabled="">
+
+      <div class="calculator-keys">
+        <button type="button" class="operator" value="+">+</button>
+        <button type="button" class="operator" value="-">-</button>
+        <button type="button" class="operator" value="*">×</button>
+        <button type="button" class="operator" value="/">÷</button>
+
+        <button type="button" value="7">7</button>
+        <button type="button" value="8">8</button>
+        <button type="button" value="9">9</button>
+
+        <button type="button" value="4">4</button>
+        <button type="button" value="5">5</button>
+        <button type="button" value="6">6</button>
+
+        <button type="button" value="1">1</button>
+        <button type="button" value="2">2</button>
+        <button type="button" value="3">3</button>
+
+        <button type="button" value="0">0</button>
+        <button type="button" class="decimal" value=".">.</button>
+        <button type="button" class="all-clear" value="all-clear">AC</button>
+
+        <button type="button" class="equal-sign operator" value="=">=</button>
+      </div>
+    </div>
+<script type="text/javascript" src="../data/js/calculator.js"></script>
+</body>
+</html>
+`;
+
+document.getElementById("calc").addEventListener("click", function() {
+    let win = window.open("", "", "width=398,height=500");
+    win.document.write(html);
+
+});
